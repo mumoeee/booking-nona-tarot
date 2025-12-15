5af6ff06be720026096ee0df768c5ef2521795ee
mumoeee <mu.moeee@gmail.com>
2025-12-15 07:21:54 +0700

    Update slot.js

--- api/slot.js
+++ api/slot.js
@@ -3,7 +3,7 @@ import { sheets } from "../lib/sheets.js";
 export default async function handler(req, res) {
   try {
     const result = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.SPREADSHEET_ID,
+      spreadsheetId: "1wMTBYpWTomcPjLn7MJuldOlZ3g6ptij0dHkEpX1-RI8",
       range: "CONFIG!A:B",
     });
 
