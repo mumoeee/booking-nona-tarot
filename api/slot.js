621e7cd62b4d11af89c28461f8ce315f22a02b7c
mumoeee <mu.moeee@gmail.com>
2025-12-15 10:29:54 +0700

    Update slot.js

--- api/slot.js
+++ api/slot.js
@@ -1,16 +1,6 @@
 import { sheets } from "../lib/sheets.js";
 
 export default async function handler(req, res) {
-  // Tambahin header CORS
-  res.setHeader("Access-Control-Allow-Origin", "*"); // biar bisa diakses dari mana aja
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); 
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  // Handle preflight request (OPTIONS)
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
   try {
     const result = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.SPREADSHEET_ID,
