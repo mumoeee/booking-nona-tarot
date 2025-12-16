1e71a6993dcf5fe66afb4867f093d63527ecc614
mumoeee <mu.moeee@gmail.com>
2025-12-17 01:13:27 +0700

    Update book.js

--- api/book.js
+++ api/book.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
     // Buat link WhatsApp otomatis
     const waNumber = '6285600045005';
     const waMessage = encodeURIComponent(
-      `Halo, saya ingin konfirmasi booking:\nNama: ${name}\nEmail: ${email}\nPaket: ${paket}\nKode Booking: ${code}\n\nBukti Pembayaran ke Rekening BRI 308101002125500 a.n. Ranni Anugrah akan saya kirim setelah pesan konfirmasi ini`
+      `Halo, saya ingin konfirmasi booking:\nNama: ${name}\nEmail: ${email}\nPaket: ${paket}\nKode Booking: ${code}\n\nBukti Pembayaran ke Rekening BRI 308101002125500 a.n. Ranni Anugrah Pramudhita akan saya kirim setelah pesan konfirmasi ini`
     );
     const waLink = `https://wa.me/${waNumber}?text=${waMessage}`;
 
